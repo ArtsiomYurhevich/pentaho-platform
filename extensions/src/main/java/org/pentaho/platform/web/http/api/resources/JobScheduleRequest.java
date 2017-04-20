8fd348050890830c8c624ea502efdda492d26c50
Artsiom Yurhevich <Artsiom_Yurhevich@epam.com>
2017-04-20 18:29:50 +0300

    java

--- extensions/src/main/java/org/pentaho/platform/web/http/api/resources/JobScheduleRequest.java
+++ extensions/src/main/java/org/pentaho/platform/web/http/api/resources/JobScheduleRequest.java
@@ -12,7 +12,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2002-2016 Pentaho Corporation..  All rights reserved.
+ * Copyright (c) 2002-2017 Pentaho Corporation..  All rights reserved.
  */
 
 package org.pentaho.platform.web.http.api.resources;
@@ -93,7 +93,7 @@ public class JobScheduleRequest implements Serializable {
 
   ArrayList<JobScheduleParam> jobParameters = new ArrayList<JobScheduleParam>();
 
-  Map<String, String> pdiParameters = new HashMap<>();
+  Map<String, String> pdiParameters;
 
   long duration;
 
